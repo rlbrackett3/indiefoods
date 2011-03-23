8bc0f4e327ac63a3a39f7af0d7ac34a6c955ce25
Robert Lee Brackett <robert.brackett3@gmail.com>
2011-03-23 17:11:13 -0400

    generated the user model with omniauth support

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
-  # :token_authenticatable, :confirmable, and :timeoutable
+  # :token_authenticatable, :confirmable, and :timeoutable, :encryptable 
   devise :database_authenticatable, :registerable, :lockable,
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable
 
