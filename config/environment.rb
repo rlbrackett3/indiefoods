f4233bd7f27614c702c5d3b074699e63e791c574
Robert Lee Brackett <robert.brackett3@gmail.com>
2011-03-23 16:50:51 -0400

    added haml options to environment file

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,10 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+# HAML Options
+Haml::Template.options[:format] = :html5
+Haml::Template.options[:escape_html] = true
+Haml::Template.options[:ugly] = true
+
 # Initialize the rails application
 Indiefoods::Application.initialize!
