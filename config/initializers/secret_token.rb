19e7b4c17f102773786e6239a9b61c0f7288429a
Robert Lee brackett III <robert.brackett3@gmail.com>
2011-03-23 11:29:02 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Indiefoods::Application.config.secret_token = 'a6984074b7fbfff162e395757f3454ce3ec1aa53203c23dfbb97890055d1b7ba80081e756c3ba1161a4b2f36132250244ab9d738616738db0496db997b13603a'
