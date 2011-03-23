6fa8c9efcd6414098ce662e12a604374586a70a0
Robert Lee Brackett <robert.brackett3@gmail.com>
2011-03-23 15:55:27 -0400

    update spec_helper for spork installed spork

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,24 @@
-# This file is copied to spec/ when you run 'rails generate rspec:install'
-ENV["RAILS_ENV"] ||= 'test'
-require File.expand_path("../../config/environment", __FILE__)
-require 'rspec/rails'
+require 'rubygems'
+require 'spork'
 
-# Requires supporting ruby files with custom matchers and macros, etc,
-# in spec/support/ and its subdirectories.
-Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+Spork.prefork do
+  
+  ENV["RAILS_ENV"] ||= 'test'
+  require File.expand_path("../../config/environment", __FILE__)
+  require 'rspec/rails'
+  require 'factory_girl'
+  require 'database_cleaner'
+  
+  # Requires supporting ruby files with custom matchers and macros, etc,
+  # in spec/support/ and its subdirectories.
+  Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+
+end
+
+Spork.each_run do
+
+
+end
 
 RSpec.configure do |config|
   # == Mock Framework
